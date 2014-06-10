f61fcb656e2ad78c24b78fbe56c6f13a41ca9877
Aditya Kotwal <akotwal@akotwal-mbpro.prom.eng.vmware.com>
2014-06-10 08:57:15 -0700

    Version 1.3    -> Alignment changes

--- dedup.c
+++ dedup.c
@@ -1,9 +1,9 @@
 
 /*
- * Author : Aditya Kotwal
- * Date of Start : 5/22/2014
- * Email : akotwal@vmware.com
- */
+   Author : Aditya Kotwal
+   Date of Start : 5/22/2014
+   Email : akotwal@vmware.com
+*/
 
 #include "dedup.h"
 
@@ -14,9 +14,9 @@ int isAccessible(char *source);
 
 
 int main(){
-    cleanDump(DST);
-    walk(SRC,dedup);
-    return 0;
+   cleanDump(DST);
+   walk(SRC,dedup);
+   return 0;
 }
 
 /*
@@ -30,13 +30,13 @@ int main(){
 
 void dedup(char *source)
 {
-    if(isAccessible(source)){
-        generateDump(source,DST);
-    }
+   if(isAccessible(source)){
+      generateDump(source,DST);
+   }
    else{
       printf("File %s not accessible\n",source);
    }
-    return;
+   return;
 }
 
 /*
@@ -47,15 +47,15 @@ void dedup(char *source)
 */
 
 void cleanDump(char *file){
-	FILE *fp=NULL;
-	fp=fopen(file,"wb");
-	if(fp==NULL){
-        	printf("Failed to open file %s in %s mode\n",file,"wb");
-    	}
-	if(fclose(fp)!=0){
-		printf("Failed to close file %s\n",file);
-	}	
-	return;
+   FILE *fp=NULL;
+   fp=fopen(file,"wb");
+   if(fp==NULL){
+      printf("Failed to open file %s in %s mode\n",file,"wb");
+   }
+   if(fclose(fp)!=0){
+      printf("Failed to close file %s\n",file);
+   }
+   return;
 }
 
 /* Generate text dump by scanning the target location
@@ -68,37 +68,37 @@ void cleanDump(char *file){
 */
 
 void generateDump(char* source, char* destination){
-    FILE *ip,*op;
-    unsigned long len;
-    unsigned long offset=0;
-    char buf[1024*4];	// No dynmaic allocation
-    char out[MD5_HASH_LEN*2+1];  // MD5_HASH_LEN stores the length of 
+   FILE *ip,*op;
+   unsigned long len;
+   unsigned long offset=0;
+   char buf[1024*4];	// No dynmaic allocation
+   char out[MD5_HASH_LEN*2+1];  // MD5_HASH_LEN stores the length of 
 				 // the encoding in bytes. When converted
 				 // to char array, each bytes will be 
 				 // represented by 2 characters. The array
 				 // will be terminated by a null char '\0'
 				 // Thus the length is 2*MD5_HASH_LEN + 1
-    ip=fopen(source,"wb");
-    if(ip==NULL){
-        printf("Failed to open file %s in %s mode\n",source,"wb");
-    }
-    op=fopen(destination,"wb");
-    if(op==NULL){
-        printf("Failed to open file %s in %s mode\n",destination,"ab");
-    }
-    fprintf(op,"String,File Name,md5,offset,size\n"); 
-    while((len=fread(buf,1,RDLEN,ip))!=0){
-	strcpy(out,"");
-        getMD5(buf,out,len);
-	fprintf(op,"%s,%s,%lu,%lu\n",source,out,offset,len);
-        offset += len;
-    }
-    if(fclose(op)!=0){
-	printf("Error closing file %s\n",destination);
-    }
-    if(fclose(ip)!=0){
-	printf("Error closing file %s\n",source);
-    }
+   ip=fopen(source,"wb");
+   if(ip==NULL){
+      printf("Failed to open file %s in %s mode\n",source,"wb");
+   }
+   op=fopen(destination,"wb");
+   if(op==NULL){
+      printf("Failed to open file %s in %s mode\n",destination,"ab");
+   }
+   fprintf(op,"String,File Name,md5,offset,size\n"); 
+   while((len=fread(buf,1,RDLEN,ip))!=0){
+      strcpy(out,"");
+      getMD5(buf,out,len);
+      fprintf(op,"%s,%s,%lu,%lu\n",source,out,offset,len);
+      offset += len;
+   }
+   if(fclose(op)!=0){
+      printf("Error closing file %s\n",destination);
+   }
+   if(fclose(ip)!=0){
+      printf("Error closing file %s\n",source);
+   }
 }
 
 /*
@@ -108,25 +108,25 @@ void generateDump(char* source, char* destination){
       - source: Absolute path of the file whose accessibility has to
 		be checked.
    Returns: 1 for success, 0 for failure (not accessible)
- */
+*/
 
 int isAccessible(char *source){
-    FILE *fp;
-    size_t size;
-    struct stat st;
-    stat(source,&st);
-    size = st.st_size;
-    if(size == 0){
-	return 0;
-    }
-    fp = NULL:
-    if((fp=fopen(source,"rb"))==NULL){
-        return 0;
-    }
-    else{
-        if(fclose(fp)!=0){
-		printf("Error closing file %s\n",source);
-	}
-        return 1;
-    }
+   FILE *fp;
+   size_t size;
+   struct stat st;
+   stat(source,&st);
+   size = st.st_size;
+   if(size == 0){
+      return 0;
+   }
+   fp = NULL:
+   if((fp=fopen(source,"rb"))==NULL){
+      return 0;
+   }
+   else{
+      if(fclose(fp)!=0){
+	 printf("Error closing file %s\n",source);
+      }
+      return 1;
+   }
 }
